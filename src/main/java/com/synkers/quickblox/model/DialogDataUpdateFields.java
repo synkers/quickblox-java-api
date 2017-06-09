d00d445872584bb547bafc01ebcac43bcfe113e8
Adam Ghani <adam1ghani@gmail.com>
2017-06-09 14:28:06 +0300

    enable editing custom data of a dialog

--- src/main/java/com/synkers/quickblox/model/DialogDataUpdateFields.java
+++ src/main/java/com/synkers/quickblox/model/DialogDataUpdateFields.java
@@ -1,24 +1,30 @@
 package com.synkers.quickblox.model;
 
+import java.util.HashMap;
+
 public class DialogDataUpdateFields {
     private String name;
     private String photo;
     private OccupantsIds push_all;
     private OccupantsIds pull_all;
+    private HashMap<String, Object> data;
+
 
     /**
-     * Updates fields of a chat
+     * Updates fields of a chat + optionally custom data
      *
-     * @param name     name of dialog
-     * @param photo    photo id
-     * @param push_all push_all operator to add new occupants
-     * @param pull_all pull_all to remove occupants
+     * @param name             name of dialog
+     * @param photo            photo id
+     * @param push_all         push_all operator to add new occupants
+     * @param pull_all         pull_all to remove occupants
+     * @param customDialogData update custom dialog data
      */
-    public DialogDataUpdateFields(String name, String photo, OccupantsIds push_all, OccupantsIds pull_all) {
+    public DialogDataUpdateFields(String name, String photo, OccupantsIds push_all, OccupantsIds pull_all, CustomDialogData customDialogData) {
         this.name = name;
         this.photo = photo;
         this.push_all = push_all;
         this.pull_all = pull_all;
+        this.data = customDialogData.assemble();
     }
 
     public String getName() {
@@ -51,5 +57,13 @@ public class DialogDataUpdateFields {
 
     public void setPull_all(OccupantsIds pull_all) {
         this.pull_all = pull_all;
+    }
+
+    public HashMap<String, Object> getData() {
+        return data;
+    }
+
+    public void setData(HashMap<String, Object> data) {
+        this.data = data;
     }
 }
