5eb53b6c3a183c54505eab8462d13ab62e32c179
Adam Ghani <adam1ghani@gmail.com>
2018-04-27 13:06:16 +0300

    refactoring QbUsersApi to take QbCallback response handler

--- src/main/java/com/synkers/quickblox/api/QbUsersApi.java
+++ src/main/java/com/synkers/quickblox/api/QbUsersApi.java
@@ -7,15 +7,9 @@ import com.synkers.quickblox.model.UserResponse;
 import com.synkers.quickblox.module.Users;
 import com.synkers.quickblox.util.QuickBlox;
 import com.synkers.quickblox.util.RetrofitUtil;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
-import java.io.IOException;
-
-public class QbUsersApi {
+public class QbUsersApi extends Api {
     private Users users;
-    private UsersCallback usersCallback;
 
     public QbUsersApi() throws AuthenticationException {
         RetrofitUtil<Users> retrofitUtil = new RetrofitUtil<Users>();
@@ -27,45 +21,18 @@ public class QbUsersApi {
         users = (Users) retrofitUtil.create(Users.class);
     }
 
-    public interface UsersCallback {
-        void onSuccess(User user);
-
-        void onFailure(String reason);
-    }
-
-    public void signUp(User user, final UsersCallback usersCallback) {
-        this.usersCallback = usersCallback;
+    public void signUp(User user, final Api.QbCallback<UserResponse> callback) {
         UserRequest userRequest = new UserRequest(user);
-        users.signUp(userRequest).enqueue(responseHandler);
+        users.signUp(userRequest).enqueue(getResponseHandler(callback));
     }
 
-    public void getUserById(String id, final UsersCallback usersCallback) {
-        this.usersCallback = usersCallback;
-        users.getUserByIdentifier(id).enqueue(responseHandler);
+    public void getUserById(String id, final Api.QbCallback<UserResponse> callback) {
+        users.getUserByIdentifier(id).enqueue(getResponseHandler(callback));
     }
 
-    public void updateUserPassword(User user, String oldPassword, String password, final UsersCallback usersCallback) {
-        this.usersCallback = usersCallback;
+    public void updateUserPassword(User user, String oldPassword, String password, final Api.QbCallback<UserResponse> callback) {
         user.setPassword(password);
         user.setOld_password(oldPassword);
-        users.updateUser(user.getId(), new UserRequest(user)).enqueue(responseHandler);
+        users.updateUser(user.getId(), new UserRequest(user)).enqueue(getResponseHandler(callback));
     }
-
-    private Callback<UserResponse> responseHandler = new Callback<UserResponse>() {
-        public void onResponse(Call<UserResponse> call, Response<UserResponse> response) {
-            if (response.isSuccessful()) {
-                usersCallback.onSuccess(response.body().getUser());
-            } else {
-                try {
-                    usersCallback.onFailure("Response Code:" + response.code() + " " + "Response Body:" + response.errorBody().string());
-                } catch (IOException e) {
-                    usersCallback.onFailure("Error getting response body" + e.toString());
-                }
-            }
-        }
-
-        public void onFailure(Call<UserResponse> call, Throwable throwable) {
-            usersCallback.onFailure(throwable.toString());
-        }
-    };
 }
